779e2abb08f0deb1b4615e185c074f86051e6dd8
25fmiflperepelytsia-ux <25fmif.l.perepelytsia@std.udu.edu.ua>
2026-06-11 10:36:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-text = input("Введіть рядок: ")
+try:
+    a = float(input("Введіть число a: "))
+    b = float(input("Введіть число b: "))
+    operation = input("Введіть знак операції (+, -, *, /): ")
 
-print(text.replace(" ", "_"))
+    if operation == "+":
+        result = a + b
+    elif operation == "-":
+        result = a - b
+    elif operation == "*":
+        result = a * b
+    elif operation == "/":
+        result = a / b
+    else:
+        raise ValueError("Некоректна операція")
+
+    print("Результат:", result)
+
+except ValueError:
+    print("Помилка: введено нечислове значення або некоректну операцію!")
+
+except ZeroDivisionError:
+    print("Помилка: ділення на нуль неможливе!")
